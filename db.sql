3b9ae293d6a6ea8c0ebc64884a8d5d223f375851
alekseykim-dev <alex.romanovich.ak@gmail.com>
2023-06-21 00:39:15 +0900

    established naver api on mariadb

--- db.sql
+++ db.sql
@@ -1,10 +1,30 @@
+-- CREATE TABLE daily (
+--   id INT AUTO_INCREMENT PRIMARY KEY,
+--   startDate DATE NOT NULL,
+--   endDate DATE NOT NULL,
+--   timeUnit VARCHAR(10) NOT NULL,
+--   keywordGroups TEXT NOT NULL,
+--   device VARCHAR(10) NOT NULL,
+--   ages TEXT NOT NULL,
+--   gender VARCHAR(1) NOT NULL
+-- );
+
+-- ALTER TABLE daily MODIFY COLUMN keywordGroups LONGTEXT;
+-- ALTER TABLE daily MODIFY COLUMN data MEDIUMTEXT NOT NULL;
+
 CREATE TABLE daily (
   id INT AUTO_INCREMENT PRIMARY KEY,
   startDate DATE NOT NULL,
   endDate DATE NOT NULL,
   timeUnit VARCHAR(10) NOT NULL,
-  keywordGroups TEXT NOT NULL,
+  groupName TEXT,
+  keywords TEXT NOT NULL,
+  data TEXT NOT NULL,
   device VARCHAR(10) NOT NULL,
   ages TEXT NOT NULL,
-  gender VARCHAR(1) NOT NULL
+  gender VARCHAR(1) NOT NULL,
+  period DATE,
+  ratio DECIMAL(10, 5)
 );
+
+
--- index.js
+++ index.js
@@ -1,87 +1,111 @@
-const express = require('express');
-const mariadb = require('mariadb');
-const request = require('request');
-const axios = require('axios');
-
+const express = require("express");
+const mariadb = require("mariadb");
+const axios = require("axios");
+const request = require("request");
 
 const app = express();
 const pool = mariadb.createPool({
-  host: 'localhost', 
-  user: 'admin', 
-  password: 'tpkris56w',
-  database: 'dynafit' ,
-  port: '3306'
+  host: "localhost",
+  user: "admin",
+  password: "tpkris56w",
+  database: "dynafit",
+  port: "3306",
 });
 
-pool.getConnection()
-  .then(connection => {
+pool
+  .getConnection()
+  .then((connection) => {
     if (connection) {
-      console.log('Connected to the database');
-      // Perform further database operations here
+      console.log("Connected to the database");
+      connection.release(); // Release the connection after checking
     }
   })
-  .catch(error => {
-    console.error('Error connecting to the database:', error);
+  .catch((error) => {
+    console.error("Error connecting to the database:", error);
   });
 
-const client_id = '010000000015943d449f90b5be258cb060a410e7a8ce075c433ea38d9f028cf5b661000087';
-const client_secret = 'AQAAAAAVlD1En5C1viWMsGCkEOeoTGUuFJeCD5SXhibYaAtWdA==';
-const api_url = 'https://openapi.naver.com/v1/datalab/search';
+const client_id = "Ly5cZOwWEEa8CD2HstRL";
+const client_secret = "1RAG48llzh";
+const api_url = "https://openapi.naver.com/v1/datalab/search";
 
-app.use(express.json());
+const request_body = {
+  startDate: "2016-01-01",
+  endDate: "2023-06-19",
+  timeUnit: "date",
+  keywordGroups: [
+    {
+      groupName: "한글",
+      keywords: ["한글"],
+    },
+  ],
+  device: "",
+  ages: ["1"],
+  gender: "",
+};
 
-app.post('/daily', async (req, res) => {
-  try {
-    const { startDate, endDate, timeUnit, keywordGroups, device, ages, gender } = req.body;
+app.listen(3000, async () => {
+  console.log("Server is running on PORT 3000");
 
-    const conn = await pool.getConnection();
-
-    // Вставка нового pull request в базу данных
-    const insertQuery = `INSERT INTO pull_requests (startDate, endDate, timeUnit, keywordGroups, device, ages, gender) VALUES (?, ?, ?, ?, ?, ?, ?)`;
-    await conn.query(insertQuery, [startDate, endDate, timeUnit, JSON.stringify(keywordGroups), device, JSON.stringify(ages), gender]);
-
-    conn.release();
-
-    const request_body = {
-        "startDate": "2016-01-01",
-        "endDate": "2023-06-19",
-        "timeUnit": "date",
-        "keywordGroups": [
-            {
-                "groupName": "한글",
-                "keywords": [
-                    "한글",
-                    "korean"
-                ]
-            },
-        ],
-        "device": "",
-        "ages": [
-        ],
-        "gender": ""
-    };
-    
-
-    request.post({
+  request.post(
+    {
       url: api_url,
       body: JSON.stringify(request_body),
       headers: {
-        'X-Naver-Client-Id': client_id,
-        'X-Naver-Client-Secret': client_secret,
-        'Content-Type': 'application/json'
-      }
+        "X-Naver-Client-Id": client_id,
+        "X-Naver-Client-Secret": client_secret,
+        "Content-Type": "application/json",
+      },
     },
-    function (error, response, body) {
-      console.log(response.statusCode);
-      console.log(body);
-      res.status(response.statusCode).json(body);
-    });
-  } catch (error) {
-    console.error('pull request error:', error);
-    res.status(500).json({ error: 'Server error' });
-  }
-});
+    async function (error, response, body) {
+      if (error) {
+        console.error("Error requesting data from API:", error);
+        return;
+      }
 
-app.listen(3000, () => {
-  console.log('SErver is running on PORT 3000');
+      if (response.statusCode === 200) {
+        const responseData = JSON.parse(body);
+
+        // Extract the relevant data from the response
+        const keywordGroups = responseData.results;
+        const data = {
+          startDate: responseData.startDate,
+          endDate: responseData.endDate,
+          timeUnit: responseData.timeUnit,
+          device: "",
+          ages: [""],
+          gender: "",
+        };
+
+        try {
+          const conn = await pool.getConnection();
+
+          // Insert data into the 'daily' table for each data point
+          const insertQuery =
+            "INSERT INTO daily (startDate, endDate, timeUnit, groupName, keywords, data, device, ages, gender) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+          for (const keywordGroup of keywordGroups) {
+            for (const { period, ratio } of keywordGroup.data) {
+              await conn.query(insertQuery, [
+                data.startDate,
+                data.endDate,
+                data.timeUnit,
+                keywordGroup.groupName || "", // Provide an empty string if groupName is undefined or null
+                JSON.stringify(keywordGroup.keywords),
+                JSON.stringify({ period, ratio }),
+                data.device,
+                JSON.stringify(data.ages),
+                data.gender,
+              ]);
+            }
+          }
+
+          conn.release();
+          console.log("Data inserted into the database");
+        } catch (error) {
+          console.error("Error inserting data into the database:", error);
+        }
+      } else {
+        console.log("API request failed with status code", response.statusCode);
+      }
+    }
+  );
 });
